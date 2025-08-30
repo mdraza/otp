5080fa66cad5571fb11a8a23fbfce85dd36e83cc
mdraza <raza.uidev@gmail.com>
2025-08-30 17:49:59 +0530

    use ref used to focus on input and takes to the next input

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,49 @@ import { useEffect, useRef, useState } from "react";
 const App = () => {
   const OTP_DIGIT_COUNT = 5;
   const [inputArr, setInputArr] = useState(new Array(OTP_DIGIT_COUNT).fill(""));
-  console.log(inputArr);
 
   const refArr = useRef([]);
 
   useEffect(() => {
-    refArr.current[0].focus();
+    refArr.current[0]?.focus();
   }, []);
 
   const handleOtp = (value, index) => {
     if (isNaN(value)) return;
 
+    const newValue = value.trim();
+
     const newArr = [...inputArr];
-    newArr[index] = value.slice(-1);
+    newArr[index] = newValue.slice(-1);
     setInputArr(newArr);
+
+    newValue && refArr.current[index + 1]?.focus();
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (!e.target.value && e.code === "Backspace") {
+      refArr.current[index - 1]?.focus();
+    }
   };
 
   return (
     <div className="main">
-      {inputArr.map((input, index) => (
-        <input
-          className="otp-input"
-          value={inputArr[index]}
-          key={index}
-          ref={(inpt) => {
-            refArr.current[index] = inpt;
-          }}
-          type="text"
-          onChange={(e) => handleOtp(e.target.value, index)}
-        />
-      ))}
+      <h2>Validate OTP</h2>
+      <div>
+        {inputArr.map((input, index) => (
+          <input
+            className="otp-input"
+            value={inputArr[index]}
+            key={index}
+            ref={(inpt) => {
+              refArr.current[index] = inpt;
+            }}
+            type="text"
+            onChange={(e) => handleOtp(e.target.value, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
